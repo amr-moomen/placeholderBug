fa4a9867a922634d89d99329dc9ec5e753bbffd2
AmrH <AmrH@DEV-AMR>
2019-12-26 17:09:16 +0200

    Add project files.

---
+++ placeholderBug/placeholderBug/Controls/HEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace placeholderBug.Controls
+{
+    public class HEntry : Entry
+    {
+
+        public HEntry()
+        {
+            this.ClearButtonVisibility = ClearButtonVisibility.WhileEditing;
+        }
+
+    }
+}
---
+++ placeholderBug/placeholderBug/MainPageArabic.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace placeholderBug
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPageArabic : ContentPage
+    {
+        public MainPageArabic()
+        {
+            InitializeComponent();
+        }
+        private void ChangeLangugeClicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
